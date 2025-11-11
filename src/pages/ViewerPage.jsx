9f0bc4f435371d36bc193d46efb1f63e1fdadc83
Huy Tran <tranquanghuy999@gmail.com>
2025-11-11 15:30:21 -0600

    set up cornerstone3D and create a basic viewer component

--- src/pages/ViewerPage.jsx
+++ src/pages/ViewerPage.jsx
@@ -1,12 +1,16 @@
 import { Box, Typography } from "@mui/material";
 import Sidebar from "../components/layout/Sidebar";
+import AxialViewer from "../components/viewer/AxialViewer";
 
 export default function ViewerPage() {
   return (
     <Box sx={{ display: "flex" }}>
       <Sidebar />
       <Box sx={{ flexGrow: 1, p: 3 }}>
-        <Typography variant="h5">MRI Viewer Area</Typography>
+        <Typography variant="h5" gutterBottom>
+          MRI Viewer Area
+        </Typography>
+        <AxialViewer />
       </Box>
     </Box>
   );
