a478f52eb5f9e2272bd7334f8732e9fb4518d46a
Huy Tran <tranquanghuy999@gmail.com>
2025-12-23 17:36:18 -0600

    move main app viewer to new page ViewerApp.js and create routes for the app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-// import TestLogin from './TestLogin';
-// import TestSignUp from './TestSignUp';
-import TestSignUpSuccess from './TestSignUpSuccess';  // Test Success page
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TestSignUpSuccess />
+    <App />
   </React.StrictMode>
 );
