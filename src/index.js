cc9ea35f8a6c73f6bf0b46d18308be04fa6cc077
Huy Tran <tranquanghuy999@gmail.com>
2025-12-23 14:23:44 -0600

    add user sign up page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import TestLogin from './TestLogin';  // Temporary for testing
+// import TestLogin from './TestLogin';
+import TestSignUp from './TestSignUp';  // Test SignUp page
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TestLogin />
+    <TestSignUp />
   </React.StrictMode>
 );
