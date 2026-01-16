fe0ccac40e78087ba8a4ed3baaddd44d20388385
Huy Tran <tranquanghuy999@gmail.com>
2026-01-15 18:44:37 -0600

    ***** REMOVE React.StrictMode to fix issues with UI being glitchy

--- src/index.js
+++ src/index.js
@@ -16,7 +16,5 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
